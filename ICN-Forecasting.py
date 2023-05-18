f099364bb4ec02a3272e765b0a99ac46aca4f977
Albert Bode <abode@illinois.edu>
2023-05-17 19:49:25 -0500

    Added FC Model

--- ICN-Forecasting.py
+++ ICN-Forecasting.py
@@ -23,14 +23,13 @@ import src.logging_utils as log_utils
 import src.analysis as analysis
 from src.config import Config
 import src.dataloader as icn_data
-from src.models import LSTM_Model, RNN_Model, GRU_Model
+from src.models import LSTM_Model, RNN_Model, GRU_Model, Dense_Model
 
 log = logging.getLogger('ICN-Forecasting')
 
 def parse_command_line():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-c', '--config', default='config.ini', action='store', help='Config file to load from')
-
     args = parser.parse_args()
     return args
 
@@ -85,117 +84,133 @@ def train(model, opt, loss_fn, train_dataloader, val_dataloader, config, tb):
     log.info('Training ended after {} epochs with train loss {tl:.2f}, val loss {vl:.2f}'.format(epoch, tl=best_train_loss, vl=best_val_loss))
     return best_model
 
-def main():
-    args = parse_command_line()
+#def main():
+args = parse_command_line()
 
-    # Start logger
-    log_utils.setup_logger('Latest.log', logging.DEBUG)
+# Start logger
+log_utils.setup_logger('Latest.log', logging.DEBUG)
 
-    # Load User Config
-    config = Config(args.config)
+# Load User Config
+config = Config(args.config)
 
-    if config.save_path != '' and not os.path.exists(os.path.join(config.save_path, 'logs')):
-        os.makedirs(os.path.join(config.save_path, 'logs'))
-    log_utils.set_log_file(os.path.join(config.save_path, 'logs', 'training.log'))
-    log_utils.set_log_level(config.debug_level)
-    # TensorBoard Logger
-    tb = SummaryWriter(os.path.join(config.save_path, 'logs'))
-    
-    # Load Dataset
-    data = icn_data.load_data(config.data_source, config.numeric_cols, config.error_cols)
+if config.save_path != '' and not os.path.exists(os.path.join(config.save_path, 'logs')):
+    os.makedirs(os.path.join(config.save_path, 'logs'))
+log_utils.set_log_file(os.path.join(config.save_path, 'logs', 'training.log'))
+log_utils.set_log_level(config.debug_level)
+# TensorBoard Logger
+tb = SummaryWriter(os.path.join(config.save_path, 'logs'))
 
-    # Pre model analysis
-    #analysis.data_analysis(data, config)
+# Load Dataset
+data = icn_data.load_data(config.data_source, config.numeric_cols, config.error_cols)
 
-    # Split into Train and Validation sets
-    # TODO better random sampling of the test and validation set.
-    thresh = round(len(data)*(1-config.validation_percent))
-    train_data = data[:thresh]
-    val_data = data[thresh:]
+# Pre model analysis
+#analysis.data_analysis(data, config)
 
-    train_x, train_y = icn_data.create_dataset(train_data[config.numeric_cols], config.prediction_target, config.training_frame_size, config.prediction_frame_size)
-    val_x, val_y = icn_data.create_dataset(val_data[config.numeric_cols], config.prediction_target, config.training_frame_size, config.prediction_frame_size)
+# Split into Train and Validation sets
+# TODO better random sampling of the test and validation set.
+thresh = round(len(data)*(1-config.validation_percent))
+train_data = data[:thresh]
+val_data = data[thresh:]
 
-    # Naive Model (Guess last years answer)
-    
-    # linear regression
+train_x, train_y = icn_data.create_dataset(train_data[config.numeric_cols], config.prediction_target, config.training_frame_size, config.prediction_frame_size)
+val_x, val_y = icn_data.create_dataset(val_data[config.numeric_cols], config.prediction_target, config.training_frame_size, config.prediction_frame_size)
 
-    # Data Prep for NN
-    train_loader = torch_data.DataLoader(torch_data.TensorDataset(train_x, train_y), shuffle=True, batch_size=config.batch_size)
-    val_loader = torch_data.DataLoader(torch_data.TensorDataset(val_x, val_y), shuffle=True, batch_size=config.validation_batchs)
-    input_shape=len(train_data[config.numeric_cols].keys())
-    model_params = {
-        'input_dim' : len(train_data[config.numeric_cols].keys()),
-        'hidden_dim' : 50,
-        'output_dim' : config.prediction_frame_size*2,
-        'layers' : 1
-    }
-    
-    # RNN
-    # Input shape = (seq_len, batch, input_size)
-    log.info('Running RNN Model')
-    rnn_model = RNN_Model(**model_params)
-    rnn_opt = optim.Adam(rnn_model.parameters(), lr=config.learning_rate)
-    loss = nn.MSELoss()
-    rnn_model = train(rnn_model, rnn_opt, loss, train_loader, val_loader, config, tb)
+# Naive Model (Guess last years answer)
 
-    # LSTM
-    log.info('Running LSTM Model')
-    lstm_model = LSTM_Model(**model_params)
-    lstm_opt = optim.Adam(lstm_model.parameters(), lr=config.learning_rate)
-    loss = nn.MSELoss()
-    lstm_model = train(lstm_model, lstm_opt, loss, train_loader, val_loader, config, tb)
+# linear regression
 
-    # GRU
-    log.info('Running GRU Model')
-    gru_model = GRU_Model(**model_params)
-    gru_opt = optim.Adam(gru_model.parameters(), lr=config.learning_rate)
-    loss = nn.MSELoss()
-    gru_model = train(gru_model, gru_opt, loss, train_loader, val_loader, config, tb)
+# Data Prep for NN
+train_loader = torch_data.DataLoader(torch_data.TensorDataset(train_x, train_y), shuffle=True, batch_size=config.batch_size)
+val_loader = torch_data.DataLoader(torch_data.TensorDataset(val_x, val_y), shuffle=True, batch_size=config.validation_batchs)
+dense_train_loader = torch_data.DataLoader(torch_data.TensorDataset(torch.flatten(train_x, start_dim=1), train_y), shuffle=True, batch_size=config.batch_size)
+dense_val_loader = torch_data.DataLoader(torch_data.TensorDataset(torch.flatten(val_x, start_dim=1), val_y), shuffle=True, batch_size=config.batch_size)
+input_shape=len(train_data[config.numeric_cols].keys())
+model_params = {
+    'input_dim' : len(train_data[config.numeric_cols].keys()),
+    'hidden_dim' : 50,
+    'output_dim' : 1,
+    'layers' : 1
+}
+dense_model_params = {
+    'input_dim' : train_x.shape[1] * train_x.shape[2],
+    'hidden_dim' : 256,
+    'output_dim' : train_y.shape[1],
+    'hidden_layers' : 1
+}
 
-    # Predictions
-    log.info('Generating prediction comparsion plots')
-    predict = {}
-    predict_x, predict_y = next(iter(val_loader))
-    predict['Orginal Series'] = predict_y[0]
-    with torch.no_grad():
-        predict['RNN'] = rnn_model(predict_x[0]).squeeze()
-        predict['LSTM'] = lstm_model(predict_x[0]).squeeze()
-        predict['GRU'] = gru_model(predict_x[0]).squeeze()
+# Fully Connected Network
+log.info('Running Dense Model')
+dense_model = Dense_Model(**dense_model_params)
+dense_opt = optim.Adam(dense_model.parameters(), lr=config.learning_rate)
+loss = nn.MSELoss()
+dense_model = train(dense_model, dense_opt, loss, dense_train_loader, dense_val_loader, config, tb)
 
-    fig, ax = analysis.plot_predictions(predict)
-    fig.savefig(os.path.join(config.save_path, 'predict.png'))
-    fig, ax = analysis.plot_error(predict)
-    fig.savefig(os.path.join(config.save_path, 'error.png'))
-    
-    # Saving Models
-    log.info('Saving Models')
-    if not os.path.exists(os.path.join(config.save_path, 'models')):
-        os.makedirs(os.path.join(config.save_path, 'models'))
-    torch.save(rnn_model, os.path.join(config.save_path, 'models', 'RNN_model.pk'))
-    torch.save(lstm_model, os.path.join(config.save_path, 'models', 'LSTM_model.pk'))
-    torch.save(gru_model, os.path.join(config.save_path, 'models', 'GRU_model.pk'))
+# RNN
+# Input shape = (seq_len, batch, input_size)
+log.info('Running RNN Model')
+rnn_model = RNN_Model(**model_params)
+rnn_opt = optim.Adam(rnn_model.parameters(), lr=config.learning_rate)
+loss = nn.MSELoss()
+rnn_model = train(rnn_model, rnn_opt, loss, train_loader, val_loader, config, tb)
 
-    # ARMA (Auto Regressive Moving Average) model
-    #arima_model = sm.tsa.ARIMA(data, order=(1,1)).fit()
+# LSTM
+log.info('Running LSTM Model')
+lstm_model = LSTM_Model(**model_params)
+lstm_opt = optim.Adam(lstm_model.parameters(), lr=config.learning_rate)
+loss = nn.MSELoss()
+lstm_model = train(lstm_model, lstm_opt, loss, train_loader, val_loader, config, tb)
 
-    # ARIMA (Auto Regressive Integrated Moving Average) model
-    # (p,q,d) p is ar q is ma d is the number of differencing required to make the time series stationary
-    #arima_model = sm.tsa.ARIMA(train[config.prediction_target], order=(1,1,0)).fit()
+# GRU
+log.info('Running GRU Model')
+gru_model = GRU_Model(**model_params)
+gru_opt = optim.Adam(gru_model.parameters(), lr=config.learning_rate)
+loss = nn.MSELoss()
+gru_model = train(gru_model, gru_opt, loss, train_loader, val_loader, config, tb)
 
-    # SARIMA
+# Predictions
+log.info('Generating prediction comparsion plots')
+predict = {}
+predict_x, predict_y = next(iter(val_loader))
+predict['Orginal Series'] = predict_y[0]
+with torch.no_grad():
+    predict['Dense'] = dense_model(torch.flatten(predict_x[0])).squeeze()
+    predict['RNN'] = rnn_model(predict_x[0]).squeeze()
+    predict['LSTM'] = lstm_model(predict_x[0]).squeeze()
+    predict['GRU'] = gru_model(predict_x[0]).squeeze()
 
-    # SARIMAX
+fig, ax = analysis.plot_predictions(predict)
+fig.savefig(os.path.join(config.save_path, 'predict.png'))
+fig, ax = analysis.plot_error(predict)
+fig.savefig(os.path.join(config.save_path, 'error.png'))
 
-    # TBATS
+# Saving Models
+log.info('Saving Models')
+if not os.path.exists(os.path.join(config.save_path, 'models')):
+    os.makedirs(os.path.join(config.save_path, 'models'))
+torch.save(rnn_model, os.path.join(config.save_path, 'models', 'RNN_model.pk'))
+torch.save(lstm_model, os.path.join(config.save_path, 'models', 'LSTM_model.pk'))
+torch.save(gru_model, os.path.join(config.save_path, 'models', 'GRU_model.pk'))
 
-    #eval_model()
+# ARMA (Auto Regressive Moving Average) model
+#arima_model = sm.tsa.ARIMA(data, order=(1,1)).fit()
 
-    # Augmented Dickey-Fuller test
-    #sm.tsa.stattools.adfuller()
+# ARIMA (Auto Regressive Integrated Moving Average) model
+# (p,q,d) p is ar q is ma d is the number of differencing required to make the time series stationary
+#arima_model = sm.tsa.ARIMA(train[config.prediction_target], order=(1,1,0)).fit()
 
-    # Close Tensorboard Writer
-    tb.close()
+# SARIMA
 
-if __name__ == '__main__':
-    main()
+# SARIMAX
+
+# TBATS
+
+#eval_model()
+
+# Augmented Dickey-Fuller test
+#sm.tsa.stattools.adfuller()
+
+# Close Tensorboard Writer
+tb.close()
+
+#if __name__ == '__main__':
+#    main()
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 def get_model(model, model_params):
     models = {
@@ -57,4 +58,24 @@ class GRU_Model(nn.Module):
     def forward(self, x):
         x, _ = self.gru(x)
         x = self.linear(x)
+        return x
+
+class Dense_Model(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim=50, hidden_layers=1):
+        super(Dense_Model, self).__init__()
+        self.model_name = 'DenseNet'
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(self.input_dim, self.hidden_dim))
+        for i in range(0,hidden_layers):
+            self.layers.append(nn.Linear(self.hidden_dim,self.hidden_dim))
+        self.layers.append(nn.Linear(self.hidden_dim, self.output_dim))
+
+    def forward(self, x):
+        for layer in self.layers[:-1]:
+            x = F.relu(layer(x))
+        x = self.layers[-1](x)
         return x
